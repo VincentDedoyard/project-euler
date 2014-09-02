e242c5f13fb2df7323f126f0ef5eed887695616d
Vincent Dedoyard <vincent.dedoyard@dsp.io>
2014-09-02 12:13:16 +0100

    Docstring with problem description.

--- problems/problem0016.py
+++ problems/problem0016.py
@@ -1,1 +1,11 @@
+# coding=utf-8
+
+"""
+215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+What is the sum of the digits of the number 2**1000?
+"""
+
+__author__ = 'Vincent Dedoyard'
+
 print sum(map(int, str(2 ** 1000)))
