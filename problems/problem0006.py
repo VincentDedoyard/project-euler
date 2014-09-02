c176a6a64373f27d7be28a2228512398b9fd3e19
Vincent Dedoyard <vincent.dedoyard@dsp.io>
2014-09-02 12:07:54 +0100

    Clean up.

--- problems/problem0006.py
+++ problems/problem0006.py
@@ -1,4 +1,4 @@
-sum_of_squared = sum([x**2 for x in range(1, 101)])
-squared_sum = sum([x for x in range(1, 101)])**2
+sum_of_squared = sum([x ** 2 for x in range(1, 101)])
+squared_sum = sum([x for x in range(1, 101)]) ** 2
 
 print squared_sum - sum_of_squared
